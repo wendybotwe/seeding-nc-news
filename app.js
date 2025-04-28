9a64c5b7f05319632f049c3f25e0003247382bf2
wendybotwe <wendy.botwe@hotmail.co.uk>
2025-04-28 17:37:21 +0100

    4-get-api-articles

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
 
-// app.get("/api/articles", getArticles);
+app.get("/api/articles", getArticles);
 
 app.get("/api/articles/:article_id", getArticleById);
 
