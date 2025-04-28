17315f33b5969f0c372b8e863974c38efb0557f4
wendybotwe <wendy.botwe@hotmail.co.uk>
2025-04-28 14:50:00 +0100

    completed 3-get-api-articles-by-article_id

--- app/models/api.models.js
+++ app/models/api.models.js
@@ -4,7 +4,23 @@ exports.selectTopics = () => {
   return db
     .query("SELECT topics.slug, topics.description FROM topics")
     .then(({ rows }) => {
-      console.log(rows, "<<< rows in the model");
       return rows;
+    });
+};
+
+exports.selectArticlebyId = (articleId) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1", [articleId])
+    .then(({ rows }) => {
+      console.log(rows, "<< rows in model");
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article_id: ${articleId}`,
+        });
+      } else {
+        console.log(rows[0], "rows[0] in model");
+        return rows[0];
+      }
     });
 };
