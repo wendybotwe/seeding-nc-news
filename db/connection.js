28ed8176dabad56b4d815f71c39cf9ed950d2db6
wendybotwe <wendy.botwe@hotmail.co.uk>
2025-05-01 09:37:25 +0100

    task 13 host application

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,18 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
-
-const db = new Pool();
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+} else {
+  console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
-
-module.exports = db;
+const config = {};
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+module.exports = new Pool(config);
--- listener.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app.js");
-
-app.listen(9090, () => {
-  console.log("Server is listening on port 9090.");
-});
